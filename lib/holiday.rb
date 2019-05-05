2e8d62e5e8424ae0956144d3a2af65ec51e6ae30
Hermann Krohn <krohnhermannacosta@gmail.com>
2019-05-05 05:49:52 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -45,7 +45,10 @@ end
 
 def all_winter_holiday_supplies(holiday_hash)
   # return an array of all of the supplies that are used in the winter season
-
+  winterOneDim = []
+  justWinter = holiday_hash[:winter]
+  winterMulDim = justWinter.values 
+  
 end
 
 def all_supplies_in_holidays(holiday_hash)
